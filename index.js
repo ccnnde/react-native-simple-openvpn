1f129eaaca100cdb848726440ad79c4907c90024
norcod <norfecod@outlook.com>
2021-08-26 15:29:43 +0800

    ✨ feat: 新增 VPN 状态监听器的添加和移除

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
-// main index.js
-
-import { NativeModules } from 'react-native';
+import { NativeModules, NativeEventEmitter } from 'react-native';
 
 const { RNSimpleOpenvpn } = NativeModules;
+const localEventEmitter = new NativeEventEmitter(RNSimpleOpenvpn);
+const stateListener = null;
+
+export const addVpnStateListener = (callback) => {
+  stateListener = localEventEmitter.addListener('stateChanged', (e) => callback(e));
+};
+
+export const removeVpnStateListener = () => {
+  if (!stateListener) {
+    return;
+  }
+  stateListener.remove();
+  stateListener = null;
+};
 
 export default RNSimpleOpenvpn;
