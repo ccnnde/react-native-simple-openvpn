9f4274227fbe8c687b9a9043a78b9c32279f5874
norcod <norfecod@outlook.com>
2021-08-30 11:03:23 +0800

    🔧 build: 添加声明文件

---
+++ index.d.ts
@@ -0,0 +1,35 @@
+interface VpnOptions {
+  remoteAddress?: string;
+  ovpnFileName?: string;
+  assetsPath?: string;
+  providerBundleIdentifier: string;
+  localizedDescription?: string;
+}
+
+interface VpnEventParams {
+  state: RNSimpleOpenvpn.VpnState;
+  message: string;
+  level?: string;
+}
+
+declare namespace RNSimpleOpenvpn {
+  function connect(options: VpnOptions): Promise<void>;
+  function disconnect(): Promise<void>;
+  function observeState(): Promise<void>;
+  function stopObserveState(): Promise<void>;
+  enum VpnState {
+    VPN_STATE_DISCONNECTED,
+    VPN_STATE_CONNECTING,
+    VPN_STATE_CONNECTED,
+    VPN_STATE_DISCONNECTING,
+    VPN_OTHER_STATE
+  }
+}
+
+declare const addVpnStateListener: (callback: (e: VpnEventParams) => void) => void;
+
+declare const removeVpnStateListener: () => void;
+
+export { addVpnStateListener, removeVpnStateListener, VpnOptions, VpnEventParams };
+
+export default RNSimpleOpenvpn;
